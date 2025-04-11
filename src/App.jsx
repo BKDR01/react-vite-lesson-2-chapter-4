d33f68be49ab4a6b20c9b26df779dd584d62c69a
BKDR01 <b0327866@gmail.com>
2025-04-11 13:36:09 +0500

    homwork

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import "./App.css";
-import LandingPage from "./componentos/header/header";
+import LandingPage from "./componentos/Header/Header.jsx";
 
 function App() {
   return (
